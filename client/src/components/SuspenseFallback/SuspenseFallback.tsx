c46c486cfb0de487d360426167f30f32d364e9bb
Mateusz MG <MateuszMG.default@com.com>
2023-08-10 12:05:05 +0200

    add Loader size

--- client/src/components/SuspenseFallback/SuspenseFallback.tsx
+++ client/src/components/SuspenseFallback/SuspenseFallback.tsx
@@ -1,10 +1,10 @@
-import { Loader } from '../global/Loader/Loader';
+import { Loader, LoaderSizes } from '../global/Loader/Loader';
 import styles from './SuspenseFallback.module.scss';
 
 export const SuspenseFallback = () => {
   return (
     <div className={styles.page}>
-      <Loader />
+      <Loader size={LoaderSizes.xxl} />
     </div>
   );
 };
